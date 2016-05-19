3fe4c2ea8e21a6bfd274f792eeed8db283b47fa2
John Spiteri <john@johnspiteri.io>
2016-05-18 21:33:16 -0400

    Updated Index Squares

--- client/scripts/index/controller.js
+++ client/scripts/index/controller.js
@@ -6,6 +6,16 @@
 		ngMeta.setTitle('Experienced Landscape Architecture');
 		ngMeta.setTag('description', 'Here is where you can edit the content on your home page.');
 
+		$scope.slides = [{
+			path: '/img/projects/residential/contemporary-court-wychwood-park/1.jpg'
+		}, {
+			path: '/img/projects/residential/formal-garden-lawrence-park/1.jpg'
+		}, {
+			path: '/img/projects/residential/modern-classic-yorkville/1.jpg'
+		}, {
+			path: '/img/projects/residential/contemporary-entry-summerhill/1.jpg'
+		}];
+
 	}
 
 	angular
